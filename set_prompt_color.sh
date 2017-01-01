aa3bd48de7ca919feb9e42b51ad05b0ca61939f8
JsonLin1995 <stu900277@gmail.com>
2017-01-01 15:20:09 +0800

    Add files via upload

---
+++ set_prompt_color.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FILES_PATH=$(grep /bin/bash /etc/passwd | awk -F ":" '{print $6 "/.bashrc"}')
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' $FILES_PATH
+source /root/.bashrc
+su $(last | awk '($8=="still" && $1!="root"){print $1}')
